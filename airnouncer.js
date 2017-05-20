603f1ec2abff712c721332dae6c75518c1878ab8
pazursky <pazursky@thinkpad.lan>
2017-05-20 21:22:37 +0200

    First iteration of working (supposedly) api.

--- airnouncer.js
+++ airnouncer.js
@@ -3,6 +3,9 @@ var cheerio = require("cheerio");
 var url = require("url");
 var http = require("http");
 
+var port = 1924;
+var host = '127.0.0.1';
+
 var arrivals = [];
 var outgoings = [];
 
@@ -23,33 +26,17 @@ server = http.createServer(function (req, res) {
             });
           };
           if (url.parse(req.url).pathname === '/airnouncer/api/getarrivals') {
-            var response = '';
+            var response = {};
             for (var q = 0; q < arrivals.length; q++) {
-              response = response + JSON.stringify(arrivals[q]) + '\n';
+              response[q.toString()] = arrivals[q];
+              // response = response + JSON.stringify(arrivals[q]) + '\n';
             }
-            res.end(response);
-            response = '';
+            res.end(JSON.stringify(response));
+            response = {};
             arrivals = [];
           };
       }
     });
 })
 
-// request('http://www.airport-poznan.com.pl/pl/', function(error, code, source) {
-//   if (!error && code.statusCode == 200){
-//       var $ = cheerio.load(source);
-//       var arrivals = $('#tabs-1');
-//       var arrivalsArr = arrivals.children();
-//       //w petli omijamy pierwszy i ostatni element, bo tam siedzi jakis syf
-//       for (var index = 1; index < arrivalsArr.length -1; index++){
-//         arrivals[index - 1] = {
-//           hour: arrivalsArr[index].children[1].children[0].data,
-//           from: arrivalsArr[index].children[3].children[0].data,
-//           nr: arrivalsArr[index].children[5].children[0].data,
-//           message: arrivalsArr[index].children[7].children[0].data
-//         };
-//       }
-//   }
-// });
-
-server.listen(1924, '127.0.0.1');
+server.listen(port, host);
